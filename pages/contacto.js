d0772f7a6c5b9980b122b273a31d6f3505fbfe51
Orlando Fernández <nandofer.11@hotmail.com>
2022-02-24 15:24:33 -0500

    Implementando Sendgrid para form contacto

--- pages/contacto.js
+++ pages/contacto.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import Layout from '../components/layout'
 import Suscripcion from '../components/suscripcion'
 
@@ -15,6 +17,96 @@ const iYouTube = <FontAwesomeIcon className="mr-2" icon={faYoutube} />
 const iPaperPlane = <FontAwesomeIcon icon={faPaperPlane}/>
 
 export default function Contacto() {
+  //Estados para los campos del formulario contacto
+  const [nombresCompleto, setNombresCompleto] = useState("");
+  const [email, setEmail] = useState("");
+  const [asunto, setAsunto] = useState("");
+  const [telefono, setTelefono] = useState("");
+  const [ciudad, setCiudad] = useState("");
+  const [mensaje, setMensaje] = useState("");
+
+  //Validación de estado del form
+  const [errors, setErrors] = useState({});
+
+  //Establecer el texto del botón en el envío del formulario
+  const [buttonText, setButtonText] = useState("Enviar");
+
+  //Configuración de estados de mensajes de éxito o fracaso
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [showFailureMessage, setShowFailureMessage] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    let isValidForm = handleValidation();
+
+    if(isValidForm){
+      setButtonText("Enviando");
+      const res = await fetch("/api/sendgrid", {
+        body: JSON.stringify({
+          nombresCompleto: nombresCompleto,
+          email: email,
+          asunto: asunto,
+          telefono: telefono,
+          ciudad: ciudad,
+          mensaje: mensaje,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+      });
+    } 
+      
+
+      const { error } = await res.json();
+      if (error) {
+        console.log(error);
+        setShowSuccessMessage(false);
+        setShowFailureMessage(true);
+        setButtonText("Enviar");
+        return;
+      }
+      setShowSuccessMessage(true);
+      setShowFailureMessage(false);
+      setButtonText("Enviar");
+    console.log(nombresCompleto, email, asunto, mensaje);
+  };
+
+  const handleValidation = () => {
+    let tempErrors = {};
+    let isValid = true;
+
+    if (nombresCompleto.length <= 0) {
+      tempErrors["nombresCompleto"] = true;
+      isValid = false;
+    }
+    if (email.length <= 0) {
+      tempErrors["email"] = true;
+      isValid = false;
+    }
+    if (asunto.length <= 0) {
+      tempErrors["asunto"] = true;
+      isValid = false;
+    }
+    if (telefono.length <= 0) {
+      tempErrors["telefono"] = true;
+      isValid = false;
+    }
+    if (ciudad.length <= 0) {
+      tempErrors["ciudad"] = true;
+      isValid = false;
+    }
+    if (mensaje.length <= 0) {
+      tempErrors["mensaje"] = true;
+      isValid = false;
+    }
+
+    setErrors({ ...tempErrors });
+    console.log("errors", errors);
+    return isValid;
+  };
+
   return (
     <div>
       <Layout
@@ -44,32 +136,32 @@ export default function Contacto() {
               </div>
             </div>
             <div className='bg-orangePrimary p-6 bg-bgForm bg-no-repeat bg-cover'>
-              <form className=''>
+              <form className='' onSubmit={handleSubmit}>
                 <div className="flex flex-col mb-3">
-                  <label className='text-whitePrimary'>Nombres y Apellidos:</label>
-                  <input type="text" className="w-96 form-input" />
+                  <label htmlFor='nombresCompleto' className='text-whitePrimary'>Nombres y Apellidos:</label>
+                  <input value={nombresCompleto} onChange={(e)=>{setNombresCompleto(e.target.value);}} name='nombresCompleto' type="text" className="w-96 form-input" />
                 </div>
                 <div className="flex flex-col mb-3">
-                  <label className='text-whitePrimary'>Correo electrónico:</label>
-                  <input type="email" className="w-96" />
+                  <label htmlFor='email' className='text-whitePrimary'>Correo electrónico:</label>
+                  <input value={email} onChange={(e)=>{setEmail(e.target.value);}} name='email' type="email" className="w-96" />
                 </div>
                 <div className="flex flex-col mb-3">
-                  <label className='text-whitePrimary'>Asunto:</label>
-                  <input type="text" className="w-96" />
+                  <label htmlFor='asunto' className='text-whitePrimary'>Asunto:</label>
+                  <input value={asunto} onChange={(e)=>{setAsunto(e.target.value);}} name='asunto' type="text" className="w-96" />
                 </div>
                 <div className='flex'>
                   <div className="flex flex-col mb-3">
-                    <label className='text-whitePrimary'>Teléfono:</label>
-                    <input type="tel" className="w-44 mr-8" />
+                    <label htmlFor='telefono' className='text-whitePrimary'>Teléfono:</label>
+                    <input value={telefono} onChange={(e)=>{setTelefono(e.target.value);}} name='telefono' type="tel" className="w-44 mr-8" />
                   </div>
                   <div className="flex flex-col mb-3">
-                    <label className='text-whitePrimary'>Ciudad:</label>
-                    <input type="text" className="w-44" />
+                    <label htmlFor='ciudad' className='text-whitePrimary'>Ciudad:</label>
+                    <input value={ciudad} onChange={(e)=>{setCiudad(e.target.value);}} name='ciudad' type="text" className="w-44" />
                   </div>
                 </div>
                 <div className="flex flex-col">
-                  <label className='text-whitePrimary'>Mensaje:</label>
-                  <textarea name="mensaje" rows="6" placeholder='Escribe tu mensaje ... '></textarea>
+                  <label htmlFor='mensaje' className='text-whitePrimary'>Mensaje:</label>
+                  <textarea value={mensaje} onChange={(e)=>{setMensaje(e.target.value);}} name="mensaje" rows="6" placeholder='Escribe tu mensaje ... '></textarea>
                 </div>
                 <a href="" className='inline-block bg-blackPrimary py-2 px-4 text-whitePrimary mt-5'>{iPaperPlane} Enviar</a>
               </form>
